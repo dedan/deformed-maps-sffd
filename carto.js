e10e0ddb9d5d9108032073a7ddb57e318ab67558
Stephan Gabler <stephan.gabler@gmail.com>
2015-02-23 18:03:27 -0800

    add random distortion of neighborhoods

--- carto.js
+++ carto.js
@@ -19,39 +19,42 @@ var g = svg.append("g")
 
 var color = d3.scale.category20b();
 
-// Load and display SF
-d3.json("bayarea_topo.json", function(error, data) {
-svg.append("g")
-      .attr("class", "SF")
-    .selectAll("path")
-      .data(topojson.feature(data, data.objects.bayarea).features)
-    .enter().append("path")
-      .attr("class", function(d){ return d.id.replace(/ /g, "_")})
-      .attr("d", path)
-});
+// // Load and display SF
+// d3.json("bayarea_topo.json", function(error, data) {
+// svg.append("g")
+//       .attr("class", "SF")
+//     .selectAll("path")
+//       .data(topojson.feature(data, data.objects.bayarea).features)
+//     .enter().append("path")
+//       .attr("class", function(d){ return d.id.replace(/ /g, "_")})
+//       .attr("d", path)
+// });
 
-// Load and display streets
-d3.json("SF_Streets_topo.json", function(error, Streets) {
-  svg.append("g")
-      .attr("class", "SF_Streets")
-    .selectAll("path")
-      .data(topojson.feature(Streets, Streets.objects.SF_Streets).features)
-    .enter().append("path")
-      .style("stroke", function(d){ return color(d.properties.street)})
-      .style("stroke-width", "0.3")
-      .attr("d", path)
-});
+// // Load and display streets
+// d3.json("SF_Streets_topo.json", function(error, Streets) {
+//   svg.append("g")
+//       .attr("class", "SF_Streets")
+//     .selectAll("path")
+//       .data(topojson.feature(Streets, Streets.objects.SF_Streets).features)
+//     .enter().append("path")
+//       .style("stroke", function(d){ return color(d.properties.street)})
+//       .style("stroke-width", "0.3")
+//       .attr("d", path)
+// });
+
+var carto = d3.cartogram()
+ .projection(projection)
+ .value(function(d) {
+   return Math.random() * 100;
+ });
 
 d3.json("sf.json", function(error,sf) {
 
-    var hoods = topojson.feature(sf, sf.objects.neighborhoods).features;
-    svg.append("path")
-        .datum(topojson.mesh(sf, sf.objects.neighborhoods, function(a, b){ return a !== b; }))
-        .attr("d", path)
-        .attr("class", "border");
+    var hoods = carto(sf, sf.objects.neighborhoods).features;
+
     svg.selectAll(".hood")
         .data(hoods)
         .enter().append("path")
         .attr("class", function(d) { return "hood " + d.id; })
-        .attr("d", path);
+        .attr("d", carto.path);
 })
