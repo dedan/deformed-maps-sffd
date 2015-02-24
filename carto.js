d18c8866c954bacd57fd6029998b943ebb829e64
Stephan Gabler <stephan.gabler@gmail.com>
2015-02-24 14:45:41 -0800

    create a Makefile to download and process all the data
    - will combine all geo information into one file
    -> delete all geoinformation from repo, will be created dynamically

--- carto.js
+++ carto.js
@@ -4,7 +4,7 @@ var margin = {top: 50, right: 30, bottom: 30, left: 40},
 
 var projection = d3.geo.mercator()
     .center([-122.4, 37.775])
-    .scale(237000)
+    .scale(200000)
     .rotate([0,0]);
 
 var svg = d3.select("body").append("svg")
@@ -19,28 +19,16 @@ var g = svg.append("g")
 
 var color = d3.scale.category20b();
 
-// // Load and display SF
-// d3.json("bayarea_topo.json", function(error, data) {
-// svg.append("g")
-//       .attr("class", "SF")
-//     .selectAll("path")
-//       .data(topojson.feature(data, data.objects.bayarea).features)
-//     .enter().append("path")
-//       .attr("class", function(d){ return d.id.replace(/ /g, "_")})
-//       .attr("d", path)
-// });
-
-// // Load and display streets
-// d3.json("SF_Streets_topo.json", function(error, Streets) {
-//   svg.append("g")
-//       .attr("class", "SF_Streets")
-//     .selectAll("path")
-//       .data(topojson.feature(Streets, Streets.objects.SF_Streets).features)
-//     .enter().append("path")
-//       .style("stroke", function(d){ return color(d.properties.street)})
-//       .style("stroke-width", "0.3")
-//       .attr("d", path)
-// });
+// Load and display SF outline
+d3.json("sf_geo.json", function(error, data) {
+svg.append("g")
+      .attr("class", "SF")
+    .selectAll("path")
+      .data(topojson.feature(data, data.objects.bayarea).features)
+    .enter().append("path")
+      .attr("class", function(d){ return d.id.replace(/ /g, "_")})
+      .attr("d", path)
+});
 
 var carto = d3.cartogram()
  .projection(projection)
@@ -48,13 +36,26 @@ var carto = d3.cartogram()
    return Math.random() * 100;
  });
 
-d3.json("sf.json", function(error,sf) {
+// Load and display streets
+d3.json("SF_Streets_topo.json", function(error, Streets) {
+  svg.append("g")
+      .attr("class", "SF_Streets")
+    .selectAll("path")
+      .data(topojson.feature(Streets, Streets.objects.SF_Streets).features)
+    .enter().append("path")
+      .style("stroke", function(d){ return color(d.properties.street)})
+      .style("stroke-width", "0.3")
+      .attr("d", path)
+});
 
-    var hoods = carto(sf, sf.objects.neighborhoods).features;
-
-    svg.selectAll(".hood")
-        .data(hoods)
-        .enter().append("path")
-        .attr("class", function(d) { return "hood " + d.id; })
-        .attr("d", carto.path);
-})
+// Load and display tracts
+d3.json("sf_geo.json", function(error, tracts) {
+  svg.append("g")
+      .attr("class", "tract")
+    .selectAll("path")
+      .data(carto(tracts, tracts.objects.tracts.geometries).features)
+    .enter().append("path")
+      // .style("stroke", function(d){ return color(d.properties.street)})
+      // .style("stroke-width", "0.3")
+      .attr("d", carto.path)
+});
